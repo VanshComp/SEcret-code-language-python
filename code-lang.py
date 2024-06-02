4c60d500329a05fc23ff42b77de6c062107cff27
VanshComp <vanshgautam2005@gamil.com>
2024-06-02 20:43:04 +0530

    Improvised version

--- code-lang.py
+++ code-lang.py
@@ -1,36 +1,39 @@
 import random
-i="c"
-#coding
-while i=="c":
-    choice=input("Enter \"code\" to form code language or \"decode\" to decode your coded language: ")
-    if(choice=="code"):
-        print("Instructions: if the word contains atleast 3 characters, remove the first letter and append it at the end now append three random characters at the starting and the end\nelse:   simply reverse the string")
-        t= list(input("Enter your text: "))
-        if len(t) == 3:
-            t[0],t[2]=t[2],t[0]
-            t1="".join(t)
-            t2="".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",k=3))
-            t3="".join(random.choices("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",k=3))
-            print( ("%s%s%s") % (t2,t1,t3))
-        else:
-            t.reverse()
-            t1="".join(t)
-            print(t1)
-#decoding
-    elif(choice=="decode"):
-        print("Instruction: if the word contains less than 3 characters, reverse it\nelse: remove 3 random characters from start and end. Now remove the last letter and append it to the beginning")
-        r= list(input("Enter your text: "))
-        if len(r) < 3:
-            r.reverse()
-            r1="".join(r)
-            print(r1)
-   
-        else:
-            r1=r[3:-3]
-            r1[0],r1[2]=r1[2],r1[0]
-            r2="".join(r1)
-            print(r2)
+
+def encode(word):
+    if len(word) >= 3:
+        random_chars = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=3))
+        random_chars_i = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=3))
+        return random_chars + word[1:] + word[0] + random_chars_i
     else:
-        print("You selected wrong path....Please choose again")
-    i=input("Press c to continue else q to quit: ")
-print("Successfully exited from program")
+        return word[::-1]
+
+def decode(word):
+    if len(word) >= 6: 
+        random_chars = word[:3]  
+        random_chars_i= word[-3:-1]
+        return word[-4] + word[3:-4]  
+    else:
+        return word[::-1]
+
+def main():
+    while True:
+        print("Enter to Code-lang game: \nHere U can encode or decode any statement u want!!! \n\nSimple chose encode to convert your secrets into code language ..... that u want\n\nAnd chose decode to retrieve it back for code language...")
+        choice = input("Enter 1 to encode or 0 to decode: ")
+        if choice == '1':
+            message = input("Enter message: ")
+            encoded_message = ' '.join(encode(word) for word in message.split())
+            print("Encoded message:", encoded_message)
+        elif choice == '0':
+            message = input("Enter message: ")
+            decoded_message = ' '.join(decode(word) for word in message.split())
+            print("Decoded message:", decoded_message)
+        else:
+            print("Wrong choice!")
+            continue
+        choice = input("Enter 1 to start again or 0 to exit: ")
+        if choice == '0':
+            break
+
+if __name__ == "__main__":
+    main()
